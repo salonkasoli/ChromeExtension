3ac19fa3f230fab515fee0061fef768b19906d4c
Ivan Salosin <salonkasoli@gmail.com>
2016-11-10 19:56:42 +0700

    Options actually saves the data

--- ext/options.js
+++ ext/options.js
@@ -1,6 +1,13 @@
 function saveOptions (){
     var activity = document.getElementById('activity').value;
-    alert (activity);
+    chrome.storage.sync.set({'act': activity}, function() {
+        });   
+    chrome.storage.sync.get('act', function(obj) {
+        document.getElementById('status').innerHTML = obj['act'];
+    });
 }
 
 document.getElementById('submit').onclick = saveOptions;
+chrome.storage.sync.get('act', function(obj) {
+    document.getElementById('status').innerHTML = obj['act'];
+});
