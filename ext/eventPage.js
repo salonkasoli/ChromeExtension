4efcf77a86e70158a8d19ea81b49cd99e2454a30
Ivan Salosin <salonkasoli@gmail.com>
2016-11-12 23:50:10 +0700

    Added block activity in eventPage

--- ext/eventPage.js
+++ ext/eventPage.js
@@ -1,11 +1,24 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-   if(changeInfo.url){
+    if(changeInfo.url){
+          
+        var urlHash = new URL(changeInfo.url).hash;
        
-       var urlHash = new URL(changeInfo.url).hash;
-       
-       if(urlHash != DOMPurify.sanitize(urlHash)){
-                  
-           alert("YOU HAVE BEEN HACKED");
+        if(urlHash != DOMPurify.sanitize(urlHash)){
+//            alert("HACKED");
+            chrome.storage.sync.get('act', function(obj) {
+                var activity = obj['act'];
+                
+                if (activity == "block"){
+                chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+                    chrome.tabs.update(tab.id, {url: "block.html"});
+                });
+                }
+                
+                else if (activity == "ignore"){
+                }
+                
+            });
+
        }
    }
    
